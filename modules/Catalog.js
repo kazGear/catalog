d8f1c3d45778a3e1297369ea8691548a5c7873e0
kazGear <165551135+kazGear@users.noreply.github.com>
2024-07-27 01:05:21 +0900

    経歴　json読込み対応

--- modules/Catalog.js
+++ modules/Catalog.js
@@ -1,6 +1,22 @@
-const FILTER = document.querySelector("#backFilter");
-const PARENT_WINDOW = document.querySelector("body");
-const MODAL_WINDOW = document.querySelector("#characterWindow");
+const PELIOD_STR_LENGTH = 7;
+const HALF_SPACE = "&nbsp;";
+const WHOLE_SPACE = "&emsp;";
+
+const openWindow = () => {
+    $("#characterButton").click(() => {
+        $("#backFilter").show();
+        $("#modalWindow").show();
+        $("body").css({"overflow": "hidden"});
+    });
+};
+
+const closeWindow = () => {
+    $("#characterCloseButton").click(() => {
+        $("#backFilter").hide();
+        $("#modalWindow").hide();
+        $("body").css({"overflow": "scroll"});
+    });
+};
 
 const calcBirthDay = () => {
     const birthDay = new Date("1987/08/23");
@@ -12,42 +28,181 @@ const calcBirthDay = () => {
         birthDay.getDate()
     );
 
-    // おおまかに年齢を算出
+    // おおまかに年齢を算出、調整
     let age = today.getFullYear() - birthDay.getFullYear();
-
-    // 調整
     if (today < thisYearBirthDay) age--;
 
-    const domAge = document.querySelector("#age");
-    domAge.textContent = age;
-};
-
-const openWindow = () => {
-    const button = document.querySelector("#characterButton");
-
-    button.addEventListener("click", () => {
-        FILTER.style.display = "block";
-        MODAL_WINDOW.style.display = "block";
-        PARENT_WINDOW.style.overflow = "hidden";
-    });
-};
-
-const closeWindow = () => {
-    const button = document.querySelector("#characterCloseButton");
-
-    button.addEventListener("click", () => {
-        FILTER.style.display = "none";
-        MODAL_WINDOW.style.display = "none";
-        PARENT_WINDOW.style.overflow = "scroll";
-    });
+    $("#age").text(age);
 };
 
 const textDecoration = () => {
     const decoLeft = "【";
     const decoRight = "】";
-    const texts = document.querySelectorAll(".decoration");
 
-    texts.forEach((text) => {
-        texts.innerText = decoLeft + text.innerText + decoRight;
+    $(".decoration").each((index, element) => {
+        console.log(`${index}: ${element.innerHTML}`)
+        element.innerHTML = decoLeft + element.innerHTML + decoRight;
     });
 };
+
+const createJobCareer = () => {
+    // 日付文字列 降順ソート (key: 期間の最終年月, 例: 2024/08 < 2024/10)
+    const historiesJson = [...histories].sort(
+        (a, b) => b.period.substring(b.period.length - PELIOD_STR_LENGTH).localeCompare(a.period.substring(a.period.length - PELIOD_STR_LENGTH))
+    );
+
+    // DOM構築
+    let historyNo = 1;
+    for (const history of historiesJson) {
+        const titleHTML = $(`<h1 class="historyTitle">${HALF_SPACE}#${HALF_SPACE}${historyNo}${WHOLE_SPACE}${history.historyTitle}</h1>`);
+
+        const historyDetailHTML = $(`<div class="historyDetail"></div>`);
+        historyDetailHTML.append(`<p><span class="detailTitle">期間：</span>${history.period}</p>`); // 期間
+        historyDetailHTML.append(`<p><span class="detailTitle">業種：</span>${history.industry}</p>`); // 業種
+        historyDetailHTML.append(`<p><span class="detailTitle">規模：</span>${history.scale}</p>`); // 規模
+        historyDetailHTML.append(`<p><span class="detailTitle">使用言語等：</span>${history.programmingLanguages}</p>`); // 使用言語等
+
+        const jobContentHTML = $(`<div class="jobContent"></div>`);
+        jobContentHTML.append(`<p class="detailTitle">業務内容</p>`);
+        jobContentHTML.append(`<div><p>${history.jobContents}</p></div>`);
+        historyDetailHTML.append(jobContentHTML);
+
+        const historyHTML = $(`<div class="history"></div>`);
+        historyHTML.append(titleHTML);
+        historyHTML.append(historyDetailHTML);
+
+        $("#rightBlock").append(historyHTML);
+        historyNo++;
+    }
+}
+
+/***********************************************************
+ * jsons
+ *
+ * jobCareer: 職務経歴
+ * readBooks: 読んできた本
+ ************************************************************/
+
+/**
+ * 職務経歴
+ *
+ * template
+{
+    "historyTitle": "",
+    "period": "",
+    "industry": "",
+    "scale": "",
+    "programmingLanguages": "",
+    "jobContents":
+        "<br>" +
+        "<br>" +
+        "<br>" +
+        "<br>" +
+        "<br>"
+}
+ */
+const histories = [
+    {
+        "historyTitle": "情報系アプリ（新規機能開発、機能改修、保守運用）",
+        "period": "2022.01 ~ 2022.08 ＋ 2023.01 ~ 2024.03",
+        "industry": "金融",
+        "scale": "15 ~ 20名",
+        "programmingLanguages": "html, css, javaScript, jQuery, java, springBoot, postgreSQL",
+        "jobContents":
+            "基本設計～結合テスト、保守運用、調査、ルーティンの自動化（VBA）、新人の方のコーディング指導<br>" +
+            "・新規機能の製造は４画面、３帳票、バッチ５本を担当。<br>" +
+            "・保守運用：アプリの障害対応、既存不良の解消、顧客依頼の修正作業等。<br>" +
+            "・製造範囲：WebUI、サーバ・バッチ処理、SQL<br>"
+    },
+    {
+        "historyTitle": "情報系システム（総合テスト～）",
+        "period": "2024.06 ~ 2024.08",
+        "industry": "住宅関連",
+        "scale": "30人",
+        "programmingLanguages": "C#, ASP.NET, cshtml, javaScript, SQL Server",
+        "jobContents":
+            "総合テスト、障害調査、障害対応<br>" +
+            "・障害発見件数：16件<br>" +
+            "・障害対応件数：11件 ※コーディング、単体テスト<br>"
+    },
+    {
+        "historyTitle": "経理システム（リプレイス）",
+        "period": "2022.11 ~ 2022.12",
+        "industry": "電力",
+        "scale": "30名",
+        "programmingLanguages": "java, springBoot, abap（SAP）",
+        "jobContents":
+            "詳細設計、コーディング<br>" +
+            "・リプレイス：abap言語（SAP）の仕様を調べながらjavaへ翻訳。<br>" +
+            "・コーディング：２機能担当（10,000 Step）。<br>"
+    },
+    {
+        "historyTitle": "情報システム（リプレイス）",
+        "period": "2022.09 ~ 2022.10",
+        "industry": "公共",
+        "scale": "10名",
+        "programmingLanguages": "html, css, java, springBoot, struts, mySQL, VBA, PowerShell",
+        "jobContents":
+            "調査、ツール製造、結合テスト<br>" +
+            "・ツール製造：VBA, powerShell を使用し、顧客の業務効率を改善した。<br>" +
+            "・リプレイス：フレームワークの置き換え（struts to spring）。<br>" +
+            "・調査：フレームを置き換えた影響により障害が発生しており、原因を調査。<br>"
+    },
+    {
+        "historyTitle": "WEBアプリ（新規立ち上げ、既存アプリの改修・障害対応）",
+        "period": "2021.06 ~ 2021.12",
+        "industry": "製造",
+        "scale": "１名 ※初現場。顧客側はIT技術者0人。",
+        "programmingLanguages": "html, css, javaScript, C#, VB.net, ASP.net Core, postgreSQL",
+        "jobContents":
+            "要求分析～結合テスト、保守<br>" +
+            "・VB.NETで作成された既存アプリに対し、機能の追加や改修を行った。<br>" +
+            "・WEBアプリ（案件管理用）を新規に作成。WebUI、サーバ処理、DB処理、サーバの構築などを行った。<br>" +
+            "※技術者としての初陣であるが、単独での常駐であるためプロとしての経験とは言えない。"
+    },
+    {
+        "historyTitle": "職業訓練（Webアプリ制作）",
+        "period": "2020.12 ~ 2021.04",
+        "industry": "―",
+        "scale": "チーム：４名、クラス：１５名",
+        "programmingLanguages": "java, postgreSQL, HTML, Servlet, JSP",
+        "jobContents":
+            "java, postgreSQL, HTML, Servlet, JSP の基本を学習。<br>" +
+            "最終課題のチームによるアプリ制作では、詳細設計～実装を担当した。<br>" +
+            "実装についてはほぼ一人で担う。<br>" +
+            "帰宅後、休日もひたすらコーディングに打ち込んでいた。<br>"
+    },
+    {
+        "historyTitle": "IT以外の主な経歴",
+        "period": "2017.05 ~ 2020.09",
+        "industry": "税理士業、経理",
+        "scale": "７～４０名",
+        "programmingLanguages": "VBA",
+        "jobContents":
+            "税務会計、決算・申告、監査、経理作業、巡回訪問<br>" +
+            "VBAを使用した業務効率化（業後、休暇を利用し在職中に習得）<br>"
+    },
+    {
+        "historyTitle": "帳票出力システム（新規開発）",
+        "period": "2024.04 ~ 2024.05",
+        "industry": "公共",
+        "scale": "2名",
+        "programmingLanguages": "C#, ASP.NET, ActiveReport, SQL Server",
+        "jobContents":
+            "詳細設計、コーディング、単体テスト<br>" +
+            "・５つの帳票出力を担当。<br>" +
+            "・製造範囲：帳票定義ライブラリ、サーバ処理、SQL"
+    }
+];
+
+/**
+ * 読んだ本
+ */
+const readBooks = [
+    {
+
+    },
+    {
+
+    }
+];
